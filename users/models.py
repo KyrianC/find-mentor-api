397e3610b1da7c2723bc187a43b87491215f0fb9
kyrian <ckyrian@protonmail.com>
2021-11-15 15:26:32 +0100

    added teacher model and avatar for all users

--- users/models.py
+++ users/models.py
@@ -2,5 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+def avatar_upload_path(instance, filename):
+    return f"{instance.username}/{filename}"
+
+
 class CustomUser(AbstractUser):
-    pass
+    is_teacher = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to=avatar_upload_path, null=True, blank=True)
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
